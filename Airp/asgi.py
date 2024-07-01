6b85c4d28bde8e7a61a7a8b62f3a409372ef0756
jatin737320 <pilotjatin737320777@gmail.com>
2024-07-01 22:15:41 +0530

    Add files via upload

---
+++ Airp/asgi.py
@@ -0,0 +1,32 @@
+"""
+ASGI config for Airp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+# Airp/asgi.py
+
+import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from Aeroppp.routing import websocket_urlpatterns
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Airp.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
+})
+
+#application = get_asgi_application()
+
